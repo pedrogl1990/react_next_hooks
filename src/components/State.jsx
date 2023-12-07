4fb922f442316d2d47ca189972b7abafb796cbe8
Pedro Leite <pgleite1990@gmail.com>
2023-12-07 13:47:00 +0000

    02_hooks

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -4,19 +4,21 @@ import { useState } from "react";
 function App() {
   const [valor, setValor] = useState(0);
 
-  const incrementar = () => {
-    setValor(valor + 1);
+  const incrementar = (v) => {
+    setValor(valor + v);
   };
 
-  const decrementar = () => {
-    setValor(valor - 1);
+  const decrementar = (v) => {
+    setValor(valor - v);
   };
 
   return (
     <>
       <p>Contador: {valor}</p>
-      <button onClick={incrementar}>Incrementar</button>
-      <button onClick={decrementar}>Decrementar</button>
+      <button onClick={() => incrementar(1)}>Incrementar</button>
+      <button onClick={() => incrementar(10)}>Incrementar 10x</button>
+      <button onClick={() => decrementar(1)}>Decrementar</button>
+      <button onClick={() => decrementar(10)}>Decrementar 10x</button>
     </>
   );
 }
